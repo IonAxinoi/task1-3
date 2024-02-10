ee94d122263275cf5ef0848132ea46b6416ede9b
IonAxinoi <159647940+IonAxinoi@users.noreply.github.com>
2024-02-10 20:23:51 +0200

    Add files via upload

---
+++ Task 1-4/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.Write($"{m} ");
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+
+    static void Main()
+    {
+        // Пример использования
+        int M = 1; // начальное значение
+        int N = 10; // конечное значение
+
+        PrintNaturalNumbers(M, N);
+    }
+}
